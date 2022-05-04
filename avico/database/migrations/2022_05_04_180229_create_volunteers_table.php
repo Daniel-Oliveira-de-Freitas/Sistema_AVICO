ee636097bbd4375acae38c7d43949fead33da7ae
tmelo387 <tmelo387@gmail.com>
2022-05-04 16:26:27 -0300

    migrations - user and volunteer

--- avico/database/migrations/2022_05_04_180229_create_volunteers_table.php
+++ avico/database/migrations/2022_05_04_180229_create_volunteers_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('volunteers', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('phone_number');
+            $table->string('state', 100);
+            $table->string('city', 100);
+            $table->string('profissao', 100);
             $table->timestamps();
         });
     }
@@ -32,6 +32,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('volunteers');
     }
 };
