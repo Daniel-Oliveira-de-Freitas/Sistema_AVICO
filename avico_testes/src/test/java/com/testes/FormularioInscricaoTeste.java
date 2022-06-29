66c00f39c7a427aee2c2a334d54da5d9ee983c33
Daniel Oliveira de Freitas <danisnake27@gmail.com>
2022-06-28 23:44:57 -0300

    add Associado3 e Voluntario3 campos em branco 1 espaço

--- avico_testes/src/test/java/com/testes/FormularioInscricaoTeste.java
+++ avico_testes/src/test/java/com/testes/FormularioInscricaoTeste.java
@@ -169,6 +169,58 @@ public class FormularioInscricaoTeste {
     }
 
     @Test
+    public void cadastroAssociado3() {
+        try {
+            driver.findElement(By.id("associado")).click();
+            driver.findElement(By.className(BTN_NEXT)).click();
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            driver.findElement(By.id("termos")).click();
+            driver.findElement(By.className(BTN_NEXT)).click();
+            action.sendKeys(Keys.PAGE_UP).build().perform();
+            action.sendKeys(Keys.PAGE_UP).build().perform();
+            Thread.sleep(2000);
+            driver.findElement(By.name("nome")).sendKeys(" ");
+            driver.findElement(By.name("dataNascimento")).sendKeys(" ");
+            generoRandom();
+            driver.findElement(By.name("cpf")).sendKeys(" ");
+            driver.findElement(By.name("rg")).sendKeys(" ");
+            driver.findElement(By.name("celular")).sendKeys(" ");
+            driver.findElement(By.name("telefone_residencial")).sendKeys(" ");
+            driver.findElement(By.name("email")).sendKeys(" @ .com");
+            driver.findElement(By.name("endereco")).sendKeys(" ");
+            driver.findElement(By.name("nmrEndereco")).sendKeys(" ");
+            driver.findElement(By.name("complemento")).sendKeys(" ");
+            driver.findElement(By.name("cep")).sendKeys(" ");
+            driver.findElement(By.name("bairro")).sendKeys(" ");
+            driver.findElement(By.name("cidade_uf")).sendKeys(" ");
+            driver.findElement(By.name("profissao")).sendKeys(" ");
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            String letra = condicaoRandom();
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            driver.findElement(By.className("password")).sendKeys(" ");
+            driver.findElement(By.className(BTN_NEXT)).click();
+            pagamentoRandom();
+            driver.findElement(By.className(BTN_NEXT)).click();
+            driver.findElement(By.name("cpf_rg[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            if (letra.equals("s")) {
+                driver.findElement(By.name("comprovanteMedico[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            } else if (letra.equals("f")) {
+                driver.findElement(By.name("certidaoObito[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            }
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            driver.findElement(By.name("comprovanteEndereco[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            driver.findElement(By.name("comprovanteRenda[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+          driver.findElement(By.xpath("/html/body/div/div/div/form/div[6]/button[3]")).click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
     public void cadastroVoluntario() {
         try {
             driver.findElement(By.id("voluntario")).click();
@@ -237,7 +289,7 @@ public class FormularioInscricaoTeste {
             driver.findElement(By.name("rg")).sendKeys("seleniumhqqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh");
             driver.findElement(By.name("celular")).sendKeys("cincocinconovenovesixqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh");
             driver.findElement(By.name("telefone_residencial")).sendKeys("adsdasdasdasfdsfgfgjtqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh");
-            driver.findElement(By.name("email")).sendKeys("4578965454@4566456453687.com");
+            driver.findElement(By.name("email")).sendKeys("4578965454@4566456453687.commmmm");
             driver.findElement(By.name("endereco")).sendKeys("vai a direita depois a esquerda e depois dobra e chegou qwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhq");
             driver.findElement(By.name("nmrEndereco")).sendKeys("oito oitoqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh");
             driver.findElement(By.name("complemento")).sendKeys("depois da ponte depois dos trilhosqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh");
@@ -251,6 +303,57 @@ public class FormularioInscricaoTeste {
             action.sendKeys(Keys.PAGE_DOWN).build().perform();
             Thread.sleep(1000);
             driver.findElement(By.className("password")).sendKeys("qwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh1234qwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh1234qwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqw erfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfhqwerfgdsfghryutyuifgfh1234");
+            driver.findElement(By.className(BTN_NEXT)).click();
+            pagamentoRandom();
+            driver.findElement(By.className(BTN_NEXT)).click();
+            driver.findElement(By.name("cpf_rg[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            if (letra.equals("s")) {
+                driver.findElement(By.name("comprovanteMedico[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            } else if (letra.equals("f")) {
+                driver.findElement(By.name("certidaoObito[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            }
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            driver.findElement(By.name("comprovanteEndereco[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+            driver.findElement(By.name("comprovanteRenda[]")).sendKeys("C:/selenium/img/Crash Bandicoot - The Wrath of Cortex (USA) (v1.01).bin");
+          driver.findElement(By.xpath("/html/body/div/div/div/form/div[6]/button[3]")).click();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    public void cadastroVoluntario3() {
+        try {
+            driver.findElement(By.id("voluntario")).click();
+            driver.findElement(By.className(BTN_NEXT)).click();
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            driver.findElement(By.id("termos")).click();
+            driver.findElement(By.className(BTN_NEXT)).click();
+            action.sendKeys(Keys.PAGE_UP).build().perform();
+            action.sendKeys(Keys.PAGE_UP).build().perform();
+            Thread.sleep(2000);
+            driver.findElement(By.name("nome")).sendKeys(" ");
+            driver.findElement(By.name("dataNascimento")).sendKeys(" ");
+            generoRandom();
+            driver.findElement(By.name("cpf")).sendKeys(" ");
+            driver.findElement(By.name("rg")).sendKeys(" ");
+            driver.findElement(By.name("celular")).sendKeys(" ");
+            driver.findElement(By.name("telefone_residencial")).sendKeys(" ");
+            driver.findElement(By.name("email")).sendKeys(" @ .com");
+            driver.findElement(By.name("endereco")).sendKeys(" ");
+            driver.findElement(By.name("nmrEndereco")).sendKeys(" ");
+            driver.findElement(By.name("complemento")).sendKeys(" ");
+            driver.findElement(By.name("cep")).sendKeys(" ");
+            driver.findElement(By.name("bairro")).sendKeys(" ");
+            driver.findElement(By.name("cidade_uf")).sendKeys(" ");
+            driver.findElement(By.name("profissao")).sendKeys(" ");
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            String letra = condicaoRandom();
+            action.sendKeys(Keys.PAGE_DOWN).build().perform();
+            Thread.sleep(1000);
+            driver.findElement(By.className("password")).sendKeys(" ");
             driver.findElement(By.className(BTN_NEXT)).click();
             pagamentoRandom();
             driver.findElement(By.className(BTN_NEXT)).click();
