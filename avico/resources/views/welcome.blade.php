6f492a01ac4694cf0de30dec5d5b5aaab5b8f1b7
Camila Martini <94460175+camilamartinimr@users.noreply.github.com>
2022-07-15 13:24:22 -0300

    Cards Noticias
    Ainda falta formar o carrossel.

--- avico/resources/views/welcome.blade.php
+++ avico/resources/views/welcome.blade.php
@@ -6,69 +6,52 @@
         <div class="container">
             <div class="masthead-heading text-uppercase">AVICO</div>
             <div class="masthead-subheading">Associação de Vítimas e Familiares de Vítimas da Covid-19</div>
-            <a class="btn btn-primary btn-xl text-uppercase" href="/inscricao">Se Associe a AVICO</a>
+            <a class="btn btn-primary btn-xl text-uppercase" href="/inscricao">Associe-se a AVICO</a>
         </div>
     </header>
-    <!-- Sobre-->
+   <!-- Cards Noticias-->
+   <section class="page-section" id="noticias">
+        <div class="container px-5">
+             <div class="row">
+                <div class="col-md-4 col-sm-12  pb-5">
+                <div class="card">
+                        <img src="images\assets\img\cards-noticias.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        </div>
+                </div>          
+                <div class="col-md-4 col-sm-12 pb-5"> 
+                <div class="card">
+                        <img src="images\assets\img\cards-noticias.jpg" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                            <a href="#" class="btn btn-primary">Go somewhere</a>
+                        </div>
+                        </div>
+                </div>  
+                <div class="col-md-4 col-sm-12 pb-5"> 
+                <div class="card">
+                            <img src="images\assets\img\cards-noticias.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                            </div>
+                </div>  
+                
 
-
-    <section id="carrosel-homepage">
-        <!--Carousel Wrapper-->
-        <div id="carousel-example-2" class="carousel slide" data-ride="carousel">
-            <!--Indicators-->
-
-            <!--/.Indicators-->
-            <!--Slides-->
-            <div class="carousel-inner" role="listbox">
-                <div class="carousel-item active">
-                    <div class="view">
-                        <img class="d-block w-100 img-fluid" src="{{ asset('images/assets/img/header.jpg') }}"
-                            alt="First slide">
-                        <div class="mask rgba-black-light"></div>
-                    </div>
-                    <div class="carousel-caption">
-                        <h3 class="h3-responsive">Light mask</h3>
-                        <p>First text</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <!--Mask color-->
-                    <div class="view">
-                        <img class="d-block w-100 img-fluid" src="{{ asset('images/assets/img/header-bg.jpg') }}"
-                            alt="Second slide">
-                        <div class="mask rgba-black-strong"></div>
-                    </div>
-                    <div class="carousel-caption">
-                        <h3 class="h3-responsive">Strong mask</h3>
-                        <p>Secondary text</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <!--Mask color-->
-                    <div class="view">
-                        <img class="d-block w-100 img-fluid" src="https://mdbootstrap.com/img/Photos/Slides/img%20(9).webp"
-                            alt="Third slide">
-                        <div class="mask rgba-black-slight"></div>
-                    </div>
-                    <div class="carousel-caption">
-                        <h3 class="h3-responsive">Slight mask</h3>
-                        <p>Third text</p>
-                    </div>
-                </div>
             </div>
-            <!--/.Slides-->
-            <!--Controls-->
-            <a class="carousel-control-prev" href="#carousel-example-2" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carousel-example-2" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
-            <!--/.Controls-->
+            </div>
         </div>
-        <!--/.Carousel Wrapper-->
+    </section>
+
+   <!-- Fim Cards Noticias-->
+   
     </section>
     <section class="page-section" id="services">
         <div class="container">
