51a0ffabef55682ab73a6403ca7b0c5e9598bcf2
Cassio Ceolin Jr <cassio.junior@owse.com.br>
2022-08-31 22:22:05 -0300

    Teste de segurança branch

--- avico/app/Http/Controllers/ListagemController.php
+++ avico/app/Http/Controllers/ListagemController.php
@@ -10,6 +10,7 @@ use function GuzzleHttp\Promise\all;
 
 class ListagemController extends Controller
 {
+    //teste
     public function Create(){
         $inscricoes = Registration::all();
         return view("static_views.associados.list")->with(compact('inscricoes'));
