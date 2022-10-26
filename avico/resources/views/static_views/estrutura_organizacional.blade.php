28e846f1ac16134a3fe94b29c38369d5bc2d63d0
Daniel Oliveira de Freitas <danisnake27@gmail.com>
2022-10-26 18:13:22 -0300

    troca de vices e correções implementadas

--- avico/resources/views/static_views/estrutura_organizacional.blade.php
+++ avico/resources/views/static_views/estrutura_organizacional.blade.php
@@ -10,60 +10,76 @@
         </div>
         <div class="row">
 
-            <div class="col-lg-6">
+            <div class="col-lg-12">
 
                 <div class="team-member">
                     <img class="mx-auto rounded-circle" src="{{ asset('images/assets/img/team/perfil-paola.jpeg')}}" alt="..." />
                     <h4>Paola Falceta</h4>
                     <p class="text-muted">Presidente e fundadora da Avico Brasil</p>
                     <p class="text-muted">Assistente social, defensora e pós-graduanda em Direitos Humanos com atuação na área da saúde e educação. Con-sultora na área de responsabilidade social e sustentabilidade.</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Twitter Profile"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand Facebook Profile"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Parveen Anand LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
             
             <div class="col-lg-6">
 
                 <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="{{ asset('images/assets/img/team/perfil-bruno.jpeg')}}" alt="..." />
-                    <h4>Bruno Da Rosa Lumertz</h4>
-                    <p class="text-muted">Vice-presidente e fundador da Avico Brasil</p>
-                    <p class="text-muted">Analista de políticas públicas, com formação em administração, é militante dos direitos humanos e da saúde pública.</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Twitter Profile"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen Facebook Profile"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Diana Petersen LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                    <img class="mx-auto rounded-circle" src="{{ asset('images/assets/img/team/RosangelaOSilva.png')}}" alt="..." />
+                    <h4>Rosângela O. Silva</h4>
+                    <p class="text-muted">Vice-presidente de saúde da Avico Brasil</p>
+                    <p class="text-muted">Socióloga(FESPSP), Assistente Social(Universidade Braz Cubas) - CRESS 61.689, Servidora Pública. Assiciada da Avico Brasil.</p>
                 </div>
             </div>
-            <!--<div class="col-lg-4">
+
+            <div class="col-lg-6">
+
                 <div class="team-member">
-                    <img class="mx-auto rounded-circle" src="assets/img/team/3.jpg" alt="..." />
-                    <h4>Larry Parker</h4>
-                    <p class="text-muted">Lead Developer</p>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Twitter Profile"><i class="fab fa-twitter"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker Facebook Profile"><i class="fab fa-facebook-f"></i></a>
-                    <a class="btn btn-dark btn-social mx-2" href="#!" aria-label="Larry Parker LinkedIn Profile"><i class="fab fa-linkedin-in"></i></a>
+                    <img class="mx-auto rounded-circle" src="{{ asset('images/assets/img/team/PamelaCopettiGhisleni.png')}}" alt="..." />
+                    <h4>Pâmela Copetti Ghisleni</h4>
+                    <p class="text-muted">Vice-presidente júridica da Avico Brasil</p>
+                    <p class="text-muted">Professora do Curso de Graduação em Direito da Faculdade CNEC Santo Ângelo, Advogada(OAB/RS 100.497).</p>
                 </div>
-            </div>-->
+            </div>
+            
+            <div class="col-lg-6">
+
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{ asset('images/assets/img/team/VeraLuciadeMeloAragao.png')}}" alt="..." />
+                    <h4>Vera Lúcia de Melo Aragão</h4>
+                    <p class="text-muted">Vice-presidente de Assistência Social e Previdência da Avico Brasil</p>
+                    <p class="text-muted">Professora, Pedagoga e Historiadora formada pela UERJ, com Pós graduação em Gestão Escolar, 
+                        Supervisão e Orientação Escolar pelas faculdades Estácio de Sá - RJ e Cândido Mendes.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-6">
+
+                <div class="team-member">
+                    <img class="mx-auto rounded-circle" src="{{ asset('images/assets/img/team/AcirJosePereiradeBastos.png')}}" alt="..." />
+                    <h4>Acir José Pereira de Bastos</h4>
+                    <p class="text-muted">Vice-presidente Administrativo Financeiro da Avico Brasil</p>
+                    <p class="text-muted">Empresário, Formado em Administração pela UFPR, Pós-graduado em Auditoria e MBA em Gestão Empresarioal pela FAE Business School Curitiba-PR.</p>
+                </div>
+            </div>
+            
         </div>
         <div class="row">
             <div class="col-lg-8 mx-auto text-center">
-                <h4>Diretora de Promoção da Saúde:</h4>
-                <p class="large text-muted">Ivarlete de França</p>
-                <h4>Diretora de Promoção da Assistência Social e Previdência:</h4>
-                <p class="large text-muted">Gabriela Moraes</p>
-                <h4>Diretora-Jurídica:</h4>
-                <p class="large text-muted">Letícia Woida</p>
-                <h4>Secretária-Geral:</h4>
-                <p class="large text-muted">Any Moraes</p>
-                <h4>Tesoureiro:</h4>
-                <p class="large text-muted">Nícolas Alcântara Rocha</p>
-                
-                <h2 class="padding-diretoria">Conselho Fiscal</h2>
-                <h3>Titulares:</h3>
-                <p class="large text-muted">Gilmar Loeblein Pauli, Luiz Fernando Valadares e Núria Debertolis da Motta</p>
-                <h3>Suplentes:</h3>
-                <p class="large text-muted">Carolina Tavares Oliveira Borges, Ivana Bernardes Lima e Maria Beatriz Mariante Brutto</p>
+                <h4>Ivarlete de França</h4>
+                <p class="large text-muted">Diretora de Promoção da Saúde</p>
+                <h4>Gabriela Moraes</h4>
+                <p class="large text-muted">Diretora de Promoção da Assistência Social e Previdência</p>
+                <h4>Letícia Woida</h4>
+                <p class="large text-muted">Diretora-Jurídica</p>
+                <h4>Any Moraes</h4>
+                <p class="large text-muted">Secretária-Geral</p>
+                <h4>Nícolas Alcântara Rocha</h4>
+                <p class="large text-muted">Tesoureiro</p>
+                <br><br>
+                <h3 class="section-subheading text-muted">Conselho Fiscal:</h3>
+                <h4>Gilmar Loeblein Pauli, Luiz Fernando Valadares e Núria Debertolis da Motta</h4>
+                <p class="large text-muted">Titulares</p>
+                <h4>Carolina Tavares Oliveira Borges, Ivana Bernardes Lima e Maria Beatriz Mariante Brutto</h4>
+                <p class="large text-muted">Suplentes</p>
             </div>
         </div>
     </div>
