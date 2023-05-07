75adfa37665d79182896a9369eaaebc8273fbe73
cassioJr <cassiojuniorceolin@gmail.com>
2023-05-07 16:52:25 -0300

    arruamdo responsividade do card de noticia

--- resources/views/components/card-notice.blade.php
+++ resources/views/components/card-notice.blade.php
@@ -1,13 +1,12 @@
 <div>
     <div class="card">
-        <img class="card-img-top d-block w-100" src="{!! $notice->caminho_imagem !!}"
-             alt="{!! $notice->titulo !!}">
+        <img class="card-img-top d-block w-100" src="{!! $notice->caminho_imagem !!}" alt="{!! $notice->titulo !!}" height="290"
+            width="610">
         <div class="card-body">
             <p class="datanoticia">{!! $notice->created_at->format('d-m-y') !!}</p>
-            <p class="card-text altura-linha"><b>{!! $notice->titulo !!}</b>
+            <p class="card-text altura-linha"><b>{!! Str::limit($notice->titulo, 45) !!}</b>
             </p>
-            <a class="btn btn-primary btn-sm"
-               href="{!! route('visualizar.noticia', $notice->id) !!}">Leia
+            <a class="btn btn-primary btn-sm" href="{!! route('visualizar.noticia', $notice->id) !!}">Leia
                 Mais</a>
         </div>
     </div>
